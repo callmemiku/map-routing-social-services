805e38b611e8ae5dc044bd29f6b9c5ced69c8f1f
Ilya Lityagin <ilityagin@phoenixit.ru>
2024-06-16 22:56:22 +0300

    conf

--- src/coordinator/src/main/java/ru/moscow/hackathon/coordinator/python/PythonScriptCaller.java
+++ src/coordinator/src/main/java/ru/moscow/hackathon/coordinator/python/PythonScriptCaller.java
@@ -1,15 +1,19 @@
 package ru.moscow.hackathon.coordinator.python;
 
 import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.util.ResourceUtils;
 import ru.moscow.hackathon.coordinator.dto.MultisheetXLSXDTO;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 @Component
+@Slf4j
 public class PythonScriptCaller {
 
     @Value("${app.run-from-jar}")
@@ -51,6 +55,12 @@ public class PythonScriptCaller {
                     sheetDTO.getSheetName()
             ).redirectErrorStream(true)
                     .start();
+        var inputReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        var errorReader = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+
+        inputReader.lines().forEach(e -> log.debug("[PYTHON INPUT] "));
+        errorReader.lines().forEach(e -> log.debug("[PYTHON ERROR] "));
+
         int rc = process.waitFor();
         if (rc != 0) {
             throw new IllegalStateException("RC from python code is not 0");
